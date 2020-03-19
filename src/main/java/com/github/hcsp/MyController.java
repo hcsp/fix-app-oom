d737b36ec65d344169085cf5695a7a4f000c44bf
biyanwen <53588091+biyanwen@users.noreply.github.com>
2020-03-19 14:34:05 +0800

    修复内存泄漏问题 (#10)
    修复内存泄漏问题 (#10)

--- src/main/java/com/github/hcsp/MyController.java
+++ src/main/java/com/github/hcsp/MyController.java
@@ -17,7 +17,12 @@ public class MyController {
     @GetMapping("/index")
     @ResponseBody
     public String index() {
-        aService.service(new Entity());
+        Entity entity = new Entity();
+        try {
+            aService.service(entity);
+        } finally {
+            entity.getThreadLocal().remove();
+        }
         return "OK";
     }
 }
