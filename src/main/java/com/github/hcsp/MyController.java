531e48efe0060c9fa019c608408ce46f01e5e5c4
bigerro <51319439+bigerro@users.noreply.github.com>
2020-05-31 01:29:45 +0800

    修复threadlocal内存泄漏

--- src/main/java/com/github/hcsp/MyController.java
+++ src/main/java/com/github/hcsp/MyController.java
@@ -17,7 +17,15 @@ public class MyController {
     @GetMapping("/index")
     @ResponseBody
     public String index() {
-        aService.service(new Entity());
-        return "OK";
+        Entity entity = null;
+        try {
+            entity = new Entity();
+            aService.service(entity);
+            return "OK";
+        } finally {
+            if (entity != null) {
+                entity.getThreadLocal().set(null);
+            }
+        }
     }
 }
